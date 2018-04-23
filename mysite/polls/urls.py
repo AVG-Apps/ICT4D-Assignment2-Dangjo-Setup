5168f2c5a0e8ed437286ed9f4914019ca0ae3934
Aron van Groningen <aronvgroningen@gmail.com>
2018-04-23 16:25:29 +0200

    created new view

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -2,6 +2,14 @@ from django.urls import path
 
 from . import views
 
+from django.http import HttpResponse
+
+from .models import Question
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
 
 urlpatterns = [
     # ex: /polls/
