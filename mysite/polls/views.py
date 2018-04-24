0b513b9a69bd0a2bc8b0322c1285cbada53dceef
Aron van Groningen <aronvgroningen@gmail.com>
2018-04-24 11:48:12 +0200

    added CSS styles(background, li)

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -47,16 +47,16 @@ def vote(request, question_id):
         selected_choice.save()
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
 
-
-class QuestionDetailViewTests(TestCase):
-    def test_future_question(self):
-        future_question = create_question(question_text='Future question.', days=5)
-        url = reverse('polls:detail', args=(future_question.id,))
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, 404)
-
-    def test_past_question(self):
-        past_question = create_question(question_text='Past Question.', days=-5)
-        url = reverse('polls:detail', args=(past_question.id,))
-        response = self.client.get(url)
-        self.assertContains(response, past_question.question_text)
+#
+# class QuestionDetailViewTests(TestCase):
+#     def test_future_question(self):
+#         future_question = create_question(question_text='Future question.', days=5)
+#         url = reverse('polls:detail', args=(future_question.id,))
+#         response = self.client.get(url)
+#         self.assertEqual(response.status_code, 404)
+#
+#     def test_past_question(self):
+#         past_question = create_question(question_text='Past Question.', days=-5)
+#         url = reverse('polls:detail', args=(past_question.id,))
+#         response = self.client.get(url)
+#         self.assertContains(response, past_question.question_text)
